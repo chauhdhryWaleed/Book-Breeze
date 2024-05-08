921191d7f4c85fbed8ce895938146de89713777e
Chaudhry Waleed <138867014+chauhdhryWaleed@users.noreply.github.com>
2024-05-08 22:53:37 +0500

    Add files via upload
    home page

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import './App.css'; // Remove this line if not needed
+// import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen mt-20"> {/* Added mt-20 for top margin */}
+    
+      <div className="flex flex-col items-center justify-center h-56">
+        <h2 className="text-center text-lg max-w-3/4 mb-4">Unlock the World of Words: Buy, Sell, and Trade your literary treasures with Book Breeze!</h2>
+        <button className="px-4 py-2 bg-green-500 text-white rounded-lg text-lg hover:bg-green-600">Browse Our Shop</button>
+      </div>
+      
+      <div className="flex justify-center mt-8">
+        {/* Replace 'picture2.jpg' with the actual path to your image */}
+       
+      </div>
+      
+      <footer className="text-center mt-auto py-4 bg-gray-200">
+        <p>&copy; 2024 Bookstore. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
